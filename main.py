3d7e77ab1793251ad0b315821acc1be3fa80eda1
testddoser <129606911+testddoser@users.noreply.github.com>
2024-04-03 22:54:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ bot = telebot.TeleBot('6305832366:AAE7bDANfSymlLY9R5Wb8ypbQMH3pHP3QRs')
 
 bot.send_message(1199404728, "Отправляю прокси")
 
-file_path = 'test.txt'
+file_path = 'proxy.txt'
 
 bot.send_document(1199404728, open(file_path, 'rb'))
